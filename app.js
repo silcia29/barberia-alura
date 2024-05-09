0c6bd6d20647e5c4305ddc39dd35d48c65014e7e
silcia29 <silvialvarez1@outlook.com>
2024-05-09 10:39:29 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,24 @@
+function boton_uno()    {
+    
+    window.location.href= "contacto.html"
+}
+
+function boton_dos( )   {
+
+    window.location.href= "contacto.html"
+}
+
+
+
+function boton_tres( )   {
+
+    window.location.href= "contacto.html"
+}
+
+
+function Enviar_form()  {
+
+    alert("Se ha enviado su formulario, en breve nos comunicaremos con usted")
+}
+
+
